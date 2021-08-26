6e57bd9b509733996e7831ae5ae4fb1019ba2e36
moonhy7 <moonhy7@naver.com>
2021-08-26 23:21:14 +0900

    21.8.26.Thur

--- Networking_Term_Project/src/mafia_game/ClientGuiMain.java
+++ Networking_Term_Project/src/mafia_game/ClientGuiMain.java
@@ -9,7 +9,7 @@ public class ClientGuiMain {
 			InetAddress ia = InetAddress.getLocalHost();
 			String ip_str = ia.toString();
 			String ip = ip_str.substring(ip_str.indexOf("/") + 1);
-			 new ClientGui(ip, 5592);
+			 new ClientGui(ip, 6000);
 		} catch (UnknownHostException e) {
 			e.printStackTrace();
 		}
